a88b08c97a7a60a0b5d42f5664a8022bbdbe96a4
이두산 <sxfc675@gmail.com>
2021-11-24 17:09:47 +0900

    [21.11.24] 부모 자식 간의 props 통신

---
+++ Vue_Project_learn/vue-project/src/components/PageTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>{{ title }}</h2>
+</template>
+
+<script>
+/*
+부모컴포넌트 => 자식컴포넌트로 데이터 전달하기 : props?
+
+현재 부모 NestedComponent / 자식 PageTitle 인 상황에서 
+
+부모의 <PageTitle /> 에서 title = "@" 라는 데이터를 전달하면 
+자식의 props에서 부모에서 넘어온 데이터 "@"를 {{title}}에 바인딩하는 구조
+*/
+export default {
+    props: {
+        title: {
+            type: String,
+            default: "페이지 제목입니다."
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue_Project_learn/vue-project/src/views/NestedComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <page-title title="부모 컴포넌트에서 자식 컴포넌트로 데이터 전달"/>
+  </div>
+</template>
+
+<script>
+import PageTitle from '../components/PageTitle.vue' // import component
+export default {
+  components: { PageTitle }, // 현재 컴포넌트에서 사용할 컴포넌트 등록
+}
+</script>
+
+<style>
+
+</style>
