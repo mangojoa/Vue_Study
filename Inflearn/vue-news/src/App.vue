f499e2d65dd97227dda174552560f1fac1a479c9
이두산 <sxfc675@gmail.com>
2021-11-29 13:22:32 +0900

    [21.11.29] router transition

--- Inflearn/vue-news/src/App.vue
+++ Inflearn/vue-news/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -20,5 +22,12 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+/* Router Transition */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
