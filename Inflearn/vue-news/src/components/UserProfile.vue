0fe7efce2e329bfc0ca7efa4b56d3fc1e14449f8
이두산 <sxfc675@gmail.com>
2021-11-30 15:01:21 +0900

    [21.11.30] userprofile 공통 컴포넌트화

---
+++ Inflearn/vue-news/src/components/UserProfile.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="user-container">
+        <div>
+            <i class="fas fa-user"></i>
+        </div>
+        <div class="user-discription">
+            <div>
+                {{ info.id }}
+            </div>
+          <!-- <router-link :to="`/user/${userInfo.user}`">
+            {{ userInfo.id }}
+          </router-link> -->
+          <div class="time">
+            {{ info.created }}
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {    
+        info: Object
+    },
+    // computed: {
+    //     userInfo() {
+    //     return this.$store.state.user;
+    // }
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-discription {
+  padding-left: 8px;
+}
+.time {
+  font-size: 7px;
+}
+</style>
--- Inflearn/vue-news/src/views/UserView.vue
+++ Inflearn/vue-news/src/views/UserView.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-      <p>name : {{ userInfo.id }}</p>
+    <user-profile :info="userInfo"></user-profile>
+      <!-- <p>name : {{ userInfo.id }}</p>
       <p>karma : {{ userInfo.karma }}</p>
-      <p>created : {{ userInfo.created }}</p>
+      <p>created : {{ userInfo.created }}</p> -->
   </div>
 </template>
 
 <script>
+/*
+UserView -> UserProfile 로 데이터를 전달하는 방법 
+1. props
+2. computed를 통한 데이터 전달 (거의 vuex를 통한 방법이라고 봐도 무방)
+*/
+import UserProfile from '../components/UserProfile.vue';
 export default {
+  components: {
+    UserProfile
+  },
   computed: {
     userInfo() {
       return this.$store.state.user;
